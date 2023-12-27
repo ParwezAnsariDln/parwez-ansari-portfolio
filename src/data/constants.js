4988ebbcab8af8bfa2f9ad93ef8a9753e2548003
ParwezAnsariDln <parwezzaq88@gmail.com>
2023-12-27 16:21:31 +0530

    updated my resume access to anyone

--- src/data/constants.js
+++ src/data/constants.js
@@ -10,7 +10,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/ParwezAnsariDln",
   resume:
-    "https://drive.google.com/file/d/1Dg8yV9I_bMGcVFMe-jq3DbBvYR6SB642/view?usp=drive_link",
+    "https://drive.google.com/file/d/1Dg8yV9I_bMGcVFMe-jq3DbBvYR6SB642/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/parwez-ansari-a30926217",
   twitter: "https://twitter.com/ParwezA83423893",
   insta: "https://www.instagram.com/parwez.zaq/?utm_source=qr&igshid=YzU1NGVlODEzOA%3D%3D3",
